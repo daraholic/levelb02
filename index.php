b299b532b3c0e650088584238dfeffbe58d515fa
annt <1112dara@gmail.com>
2022-01-03 09:38:37 +0800

    完成頁首頁尾搬移

--- index.php
+++ index.php
@@ -12,13 +12,9 @@
 <div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
 	<pre id="ssaa"></pre>
 </div>
-<iframe name="back" style="display:none;"></iframe>
+
 	<div id="all">
-    	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
-        <div id="title2">
-        	
-        </div>
+    	<?php include "front/header.php";?>
         <div id="mm">
         	<div class="hal" id="lef">
             	                	    <a class="blo" href="?do=po">分類網誌</a>
@@ -29,8 +25,7 @@
                	                 </div>
             <div class="hal" id="main">
             	<div>
-            		
-                	<span style="width:18%; display:inline-block;">
+            		<marquee style="width: 80%;" display="flex">請民眾踴躍投稿電子報，讓電子報成為大家相戶交流，分享的園地!詳見最新文章</marquee><span style="width:18%; display:inline-block;">
                     	                    	<a href="?do=login">會員登入</a>
                     	                    </span>
                     	<div class="">
@@ -38,11 +33,6 @@
                 </div>
             </div>
         </div>
-        <div id="bottom">
-    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
-    		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
-        </div>
-    </div>
+		<?php include "front/footer.php";?>
 
 </body></html>
